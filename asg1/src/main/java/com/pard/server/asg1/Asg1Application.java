31b06649024f15da730c7229adb69fdafe6edaae
binjiseong <9900bjs@gmail.com>
2025-03-25 22:37:20 +0900

    수정 완료

---
+++ asg1/src/main/java/com/pard/server/asg1/Asg1Application.java
@@ -0,0 +1,13 @@
+package com.pard.server.asg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asg1Application.class, args);
+	}
+
+}
---
+++ asg1/src/test/java/com/pard/server/asg1/Asg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.server.asg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
