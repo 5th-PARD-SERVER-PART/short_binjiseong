0d08a059a825d7bbee862561a118a9044780935b
qlswltjd <103317212+qlswltjd@users.noreply.github.com>
2025-05-24 07:17:57 +0900

    Delete hw5 directory

--- hw5/src/main/java/com/pard/hw5/user/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pard.hw5.user.repo;
-
-
-import com.pard.hw5.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
